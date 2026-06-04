86881715cc6e9fb1089fc4e057ce31dd8927946d
JCanales03 <jon.canales@duocuc.cl>
2026-06-04 19:03:46 -0400

    MS Recursos: Conexion con MS Incendios y con Neon DB

---
+++ src/main/java/com/conaf/microservicio/recursos/config/RecursosIniciales.java
@@ -0,0 +1,42 @@
+package com.conaf.microservicio.recursos.config;
+
+import com.conaf.microservicio.recursos.model.Recurso;
+import com.conaf.microservicio.recursos.repository.RecursoRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.List;
+
+@Configuration
+public class RecursosIniciales {
+
+    @Bean
+    CommandLineRunner initDatabase(RecursoRepository repository) {
+        return args -> {
+
+            if (repository.count() == 0) {
+
+                repository.saveAll(List.of(
+                    // 6 Brigadas disponibles
+                    new Recurso(null, "Brigada 01", "Terrestre", "Disponible",null),
+                    new Recurso(null, "Brigada 02", "Terrestre", "Disponible",null),
+                    new Recurso(null, "Brigada 03", "Terrestre", "Disponible",null),
+                    new Recurso(null, "Brigada 04", "Terrestre", "Disponible",null),
+                    new Recurso(null, "Brigada 05", "Terrestre", "Disponible",null),
+                    new Recurso(null, "Brigada 06", "Terrestre", "Disponible",null),
+                    // 2 Brigadas en descanso
+                    new Recurso(null, "Brigada 07", "Terrestre", "En descanso",null),
+                    new Recurso(null, "Brigada 08", "Terrestre", "En descanso",null),
+                    // Aviones
+                    new Recurso(null, "Avión Cisterna 01", "Aereo", "Disponible",null),
+                    new Recurso(null, "Avión Cisterna 02", "Aereo", "En mantencion",null),
+                    // Camiones
+                    new Recurso(null, "Camión Aljibe 01", "Terrestre", "Disponible",null),
+                    new Recurso(null, "Camión Aljibe 02", "Terrestre", "Disponible",null),
+                    new Recurso(null, "Camión Aljibe 03", "Terrestre", "Disponible",null),
+                    new Recurso(null, "Camión Aljibe 04", "Terrestre", "Disponible",null)
+                ));
+            }
+        };
+    }
+}
---
+++ src/main/java/com/conaf/microservicio/recursos/service/RecursoService.java
@@ -0,0 +1,64 @@
+package com.conaf.microservicio.recursos.service;
+
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.conaf.microservicio.recursos.model.Recurso;
+import com.conaf.microservicio.recursos.repository.RecursoRepository;
+
+@Service
+public class RecursoService {
+
+    @Autowired
+    private RecursoRepository recursoRepository;
+
+    @Autowired
+    private WebClient webClient;
+
+    public List<Recurso> getRecursos() {
+        return recursoRepository.findAll();
+    }
+
+    public Recurso saveRecurso(Recurso recurso) {
+        return recursoRepository.save(recurso);
+    }
+
+    public Recurso getRecursoId(Long id) {
+        return recursoRepository.findById(id).orElse(null);
+    }
+
+    public Recurso updateRecurso(Recurso recurso) {
+        return recursoRepository.save(recurso);
+    }
+
+    public String deleteRecurso(Long id) {
+        recursoRepository.deleteById(id);
+        return "Recurso eliminado";
+    }
+
+    public String asignarRecursoAIncendio(Long recursoId, Long incendioId) {
+    
+        webClient.get()
+            .uri("http://localhost:8091/api/incendios/" + incendioId)
+            .retrieve()
+            .toBodilessEntity()
+            .block();
+
+        Recurso recurso = recursoRepository.findById(recursoId)
+                            .orElseThrow(() -> new RuntimeException("Recurso no encontrado"));
+        
+        if (recurso.getIncendioId() != null) {
+        return "El recurso ya está asignado al incendio ID: " + recurso.getIncendioId();
+        }                   
+
+        recurso.setIncendioId(incendioId);
+        recurso.setEstadoRecurso("En Combate");
+        recursoRepository.save(recurso);
+
+        return "El recurso " + recurso.getNombre() + " ha sido asignado exitosamente al incendio ID: " + incendioId;
+    }
+}
+

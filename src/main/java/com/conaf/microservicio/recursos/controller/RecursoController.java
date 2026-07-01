f5093990cef637e56563d3883fd0183c9465bbdc
JCanales03 <jon.canales@duocuc.cl>
2026-07-01 19:56:57 -0400

    Arreglos en Interfaz

--- src/main/java/com/conaf/microservicio/recursos/controller/RecursoController.java
+++ src/main/java/com/conaf/microservicio/recursos/controller/RecursoController.java
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
